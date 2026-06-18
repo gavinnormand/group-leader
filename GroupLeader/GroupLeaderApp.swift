b298e57bedcb453f321185d5371141765ae6ecff
gavinnormand <normandgavin@gmail.com>
2026-06-18 11:31:10 -0400

    Initial Commit

---
+++ GroupLeader/GroupLeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupLeaderApp.swift
+//  GroupLeader
+//
+//  Created by Gavin Normand on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct GroupLeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
